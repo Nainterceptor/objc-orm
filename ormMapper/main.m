646962218c845c26c1cb1dd0d6139f7b4c89efa7
Gaël Demette <demette.gael@gmail.com>
2014-03-19 23:08:49 +0100

    Initial Commit

---
+++ ormMapper/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ormMapper
+//
+//  Created by Nainterceptor on 19/03/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
